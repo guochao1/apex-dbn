49c46dea5aca2cfaa752a9799c8fa3c7d5fb65b3
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-15 09:43:41 +0000

    a complete version of srbm

---
+++ trunk/utils/apex_task.h
@@ -0,0 +1,31 @@
+#ifndef _APEX_TASK_H_
+#define _APEX_TASK_H_
+namespace apex_utils{
+    /* interface of task program */
+    class ITask{
+    public:
+        virtual void set_param( const char *name , const char *val ) = 0;
+        virtual void set_task ( const char *task ) =0;
+        virtual void print_task_help( FILE *fo ) const = 0;
+        virtual void run_task( void )= 0;
+        virtual ~ITask(){}
+    };
+    
+    inline int run_task( int argc , char *argv[] , ITask *tsk ){
+        if( argc < 2 ){
+            tsk->print_task_help( stdout );
+            return 0;            
+        }   
+        tsk->set_task( argv[1] );
+        
+        for( int i = 2 ; i < argc ; i ++ ){
+            char name[256],val[256];
+            if( sscanf( argv[i] ,"%[^=]=%[^\n]", name , val ) == 2 ){
+                tsk->set_param( name , val );
+		}   
+        }
+        tsk->run_task();
+        return 0;
+    } 
+};
+#endif
