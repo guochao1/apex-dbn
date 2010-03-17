c1329c1cd2a087181c83e392ebbf597aa435e040
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-17 07:43:22 +0000

    fix a bug in crbm

--- trunk/crbm/apex_crbm_model_stats.h
+++ trunk/crbm/apex_crbm_model_stats.h
@@ -12,7 +12,7 @@ namespace apex_rbm{
         apex_tensor::CTensor4D grad_W;
         apex_tensor::CTensor1D pos_grad_v, pos_grad_h, neg_grad_v, neg_grad_h;
         apex_tensor::CTensor1D loss, grad_sparse;
-        int sample_counter;
+        int sample_counter, h_size, v_size;
     public:
         CRBMModelStats( int v_max, int h_max, int y_max, int x_max ){
             grad_W.set_param( v_max, h_max, y_max, x_max );
@@ -56,22 +56,24 @@ namespace apex_rbm{
         }
         
         inline void save_summary( FILE *fo ) {
+            float ss = ((float)sample_counter) * h_size;
+            float vs = ((float)sample_counter) * v_size;
+
             grad_h = pos_grad_h + neg_grad_h;
             grad_v = pos_grad_v + neg_grad_v;
-
-            fprintf( fo, "avg_loss=%f,", (float)apex_tensor::cpu_only::avg( loss ) / sample_counter );
-
+            
+            fprintf( fo, "avg_loss=%f,", (float)apex_tensor::cpu_only::avg( loss ) / vs );
             fprintf( fo,"avg_h_grad=%f, avg_v_grad=%f, avg_w_grad=%f, avg_grad_sparse=%f,", 
-                     (float)apex_tensor::cpu_only::avg( grad_h ) / sample_counter,
-                     (float)apex_tensor::cpu_only::avg( grad_v ) / sample_counter,                     
-                     (float)apex_tensor::cpu_only::avg( grad_W ) / sample_counter,
-                     (float)apex_tensor::cpu_only::avg( grad_sparse ) / sample_counter ); 
+                     (float)apex_tensor::cpu_only::avg( grad_h ) / ss,
+                     (float)apex_tensor::cpu_only::avg( grad_v ) / ss,                     
+                     (float)apex_tensor::cpu_only::avg( grad_W ) / ss,
+                     (float)apex_tensor::cpu_only::avg( grad_sparse ) / ss ); 
 
             fprintf( fo, "pos_v_mean=%f, neg_v_mean=%f, pos_h_mean=%f, neg_h_mean=%f\n", 
-                     (float)apex_tensor::cpu_only::avg( pos_grad_v ) / sample_counter,
-                     (float)apex_tensor::cpu_only::avg( neg_grad_v ) / sample_counter,                     
-                     (float)apex_tensor::cpu_only::avg( pos_grad_h ) / sample_counter,
-                     (float)apex_tensor::cpu_only::avg( neg_grad_h ) / sample_counter ); 
+                     (float)apex_tensor::cpu_only::avg( pos_grad_v ) / vs,
+                     (float)apex_tensor::cpu_only::avg( neg_grad_v ) / vs,                     
+                     (float)apex_tensor::cpu_only::avg( pos_grad_h ) / ss,
+                     (float)apex_tensor::cpu_only::avg( neg_grad_h ) / ss ); 
 
             fflush( fo );
         }                        
