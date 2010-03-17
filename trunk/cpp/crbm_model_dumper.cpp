487946205bfaafad325c0b9dcb5544be86ce0e7f
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-17 11:49:18 +0000

    add dumper and drawer

---
+++ trunk/cpp/crbm_model_dumper.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "../utils/apex_utils.h"
+#include "../tensor/apex_tensor.h"
+#include "../crbm/apex_crbm_model.h"
+
+int main( int argc, char *argv[] ){
+    if( argc < 3 ){
+        printf("Usage: <model in> <text_out>");        
+        return 0;
+    }
+	apex_rbm::CDBNModel model;
+	
+    FILE *fi = apex_utils::fopen_check( argv[1] , "rb" );
+    model.load_from_file( fi );
+    fclose( fi );
+
+    FILE *fo = apex_utils::fopen_check( argv[2] , "w" );
+    model.save_to_text( fo );
+    fclose( fo );
+    return 0;
+}
+
