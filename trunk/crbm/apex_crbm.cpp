34784f6651cd855520932651d7cf8ec24db2cd82
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-22 04:52:01 +0000

    make sparse not enter momentum

--- trunk/crbm/apex_crbm.cpp
+++ trunk/crbm/apex_crbm.cpp
@@ -279,7 +279,7 @@ namespace apex_rbm{
                 cal_sparse();
                 h_bias    = h_bias * ( 1-eta*param.wd_h ) + d_h_bias * eta;
                 // add sparse regularization
-                h_bias    -=h_sum_mf *eta; 
+                h_bias    = h_bias * 1.0f + h_sum_mf *(-eta); 
                 d_h_bias  *= param.momentum;
                 
                 h_sum_mf = 0.0f; h_sum_mf_grad = 0.0f;
