713a192cba86fbd80c008ba2f05f2a925e51ef86
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-24 06:00:47 +0000

    add kyoto

---
+++ trunk/cpp/crbm_kyoto.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "../tensor/apex_tensor.h"
+#include "../utils/data_set/apex_kyoto_iterator.h"
+#include "../utils/task/apex_tensor_update_task.h"
+#include "../crbm/apex_crbm_trainer.h"
+
+using namespace apex_utils;
+
+int main( int argc, char *argv[] ){
+	apex_tensor::init_tensor_engine_cpu( 10 );
+    KyotoIterator<apex_tensor::CTensor4D> itr;        
+    apex_rbm::CRBMTrainer trainer;
+	TensorUpdateTask<apex_tensor::CTensor4D> tsk( &trainer, &itr );	
+    return run_task( argc, argv, &tsk );
+}
+
