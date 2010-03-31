3bfe3b20d10ba4262f103e447a5f258305a74fe6
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-03-31 12:47:46 +0000

    x

--- trunk/cpp/srbm_kyoto.cpp
+++ trunk/cpp/srbm_kyoto.cpp
@@ -2,7 +2,7 @@
 #define _CRT_SECURE_NO_DEPRECATE
 
 #include "../tensor/apex_tensor.h"
-#include "../utils/data_set/apex_minist_iterator.h"
+#include "../utils/data_set/apex_kyoto_iterator.h"
 #include "../utils/task/apex_tensor_update_task.h"
 #include "../srbm/apex_srbm_trainer.h"
 
@@ -10,7 +10,7 @@ using namespace apex_utils;
 
 int main( int argc, char *argv[] ){
 	apex_tensor::init_tensor_engine_cpu( 10 );
-    MINISTIterator<apex_tensor::CTensor2D> itr;        
+    KyotoIterator<apex_tensor::CTensor2D> itr;        
     apex_rbm::SRBMTrainer trainer;
 	TensorUpdateTask<apex_tensor::CTensor2D> tsk( &trainer, &itr );	
     return run_task( argc, argv, &tsk );
