2e587a0bb6c83ccc736bdfe92763cc1ea45124cb
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-23 15:23:40 +0000

    x

---
+++ cpp/crbm_kyoto_gpubuf.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include "../tensor/apex_tensor.h"
+#include "../utils/data_set/apex_kyoto_iterator.h"
+#include "../utils/task/apex_tensor_update_task.h"
+#include "../crbm/apex_crbm_trainer.h"
+
+using namespace apex_utils;
+
+int main( int argc, char *argv[] ){
+	apex_tensor::init_tensor_engine_cpu( 10 );
+    KyotoIterator<apex_tensor::GTensor4D,apex_tensor::GTensor3D> itr;        
+    apex_rbm::CRBMTrainer<apex_tensor::GTensor4D> trainer;
+	TensorUpdateTask<apex_tensor::GTensor4D> tsk( &trainer, &itr );	
+    return run_task( argc, argv, &tsk );
+}
+
