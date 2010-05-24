21e5ffbdba7b10a8f89aba750712cc545be5cd96
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-24 03:53:04 +0000

    x

--- matlab/kyoto/check_kyoto.m
+++ matlab/kyoto/check_kyoto.m
@@ -7,10 +7,11 @@ for i = [1:num-1]
     clear OL
     x_max = fread( fi, 1, 'int32' );
     y_max = fread( fi, 1, 'int32' );
-    OL = fread( fi, [ x_max, y_max ] , 'float32' );
+    z_max = fread( fi, 1, 'int32' );
+    OL = fread( fi, [ x_max, y_max , z_max ] , 'float32' );
     OL = OL';
     if( length(OL) > 1 )
-        imshow( OL );
+        imshow( sum(OL,3) );
         pause;
     end
 end
---
+++ matlab/kyoto/mk_kyoto_from_rgb_filter.m
@@ -0,0 +1,34 @@
+function mk_kyoto_from_rgb_filter( fname, lst )
+% parse mat file and save them into a binary formatted file first
+% designed for Kyoto Dataset, the binary file can be read by a iterator
+% of RBM to do training
+
+fo = fopen( fname, 'wb');
+% write the size of file
+fwrite( fo, [ 0 ] , 'int32');
+
+count = 0;
+for i = [ 1 : length(lst) ]
+    nm = lst(i).name;
+    img = imread(nm);
+    [x_max,y_max,z_max] = size( img );
+
+    ff = [-1 0 1];
+    grad_x    = imfilter( im2double(img) ,  ff  ); 
+    grad_y    = imfilter( im2double(img) , -ff' ); 
+    if z_max > 1
+        grad_x = max( grad_x , [], 3 );
+        grad_y = max( grad_y , [], 3 );
+    end  
+           
+    fwrite( fo, [x_max,y_max,2] , 'int32'); 
+    fwrite( fo, grad_x', 'float32' );
+    fwrite( fo, grad_y', 'float32' );
+    count = count + 1;
+end
+
+frewind( fo );
+fwrite( fo, [ count ] , 'int32');
+fclose( fo );
+
+fprintf(1,'%d parsed\n',count);
