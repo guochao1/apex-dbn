640d73ed76069731c02e4dd79b74237fe63245b1
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-25 05:37:46 +0000

    data

--- matlab/hog_exp/step_make_crbm_feature.m
+++ matlab/hog_exp/step_make_crbm_feature.m
@@ -9,13 +9,13 @@ crbm_model = load_crbm_model('');
 
 
 
-fprintf(1,'generate feature for %s train_pos',fname);
+fprintf(1,'generate feature for %s train_pos\n',fname);
 feature_train_pos = gen_crbm_feature( lst_train_pos, crbm_model );
-fprintf(1,'generate feature for %s train_neg',fname);
+fprintf(1,'generate feature for %s train_neg\n',fname);
 feature_train_neg = gen_crbm_feature( lst_train_neg, crbm_model );
-fprintf(1,'generate feature for %s test_pos',fname);
+fprintf(1,'generate feature for %s test_pos\n',fname);
 feature_test_pos = gen_crbm_feature( lst_test_pos, crbm_model );
-fprintf(1,'generate feature for %s test_neg',fname);
+fprintf(1,'generate feature for %s test_neg\n',fname);
 feature_test_neg = gen_crbm_feature( lst_test_neg, crbm_model );
 
 save( fname, 'feature_train_pos', 'feature_train_neg',...
--- matlab/hog_exp/substep_make_class_feature.m
+++ matlab/hog_exp/substep_make_class_feature.m
@@ -1,8 +1,8 @@
-fprintf(1,'generate feature for %s train_pos',fname);
+fprintf(1,'generate feature for %s train_pos\n',fname);
 feature_train_pos = gen_hog_feature( lst_train_pos, param );
-fprintf(1,'generate feature for %s train_neg',fname);
+fprintf(1,'generate feature for %s train_neg\n',fname);
 feature_train_neg = gen_hog_feature( lst_train_neg, param );
-fprintf(1,'generate feature for %s test_pos',fname);
+fprintf(1,'generate feature for %s test_pos\n',fname);
 feature_test_pos = gen_hog_feature( lst_test_pos, param );
 
 save( fname, 'feature_train_pos', 'feature_train_neg',...
