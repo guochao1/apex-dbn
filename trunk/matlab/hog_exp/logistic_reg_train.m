aff91c9734aa72c2be40db7aa323b7b0ebfe5245
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-25 05:37:46 +0000

    data

---
+++ trunk/matlab/hog_exp/logistic_reg_train.m
@@ -0,0 +1,63 @@
+function [B,bias] = logistic_reg_train( G, R, ...
+                                        learning_rate, wd, momentum,...
+                                        num_iter, ...
+                                        B, bias )
+% train a linear model using logistic regression
+% G: v * h matrix, with h columns of global-featur
+% R: 1 * h matrix, true answer of prediction
+%
+% B            : 1 * v matrix global factor
+% bias         : global bias
+
+if nargin < 3
+    learning_rate = 0.01;
+    wd = 1.0;
+    momentum = 0.5; 
+end
+if nargin < 6
+    num_iter = 100;
+end
+
+[v,h] = size( G );
+
+if nagin < 8
+    B = randn( 1, n ) * 0.01;
+    bias = 0;
+end
+
+dB    = zeros( 1, v );
+dbias = 0;
+
+for iter = 1 : num_iter    
+    W = 1./(1 + exp( - B*G -bias ));    
+    E = R - W;
+
+    if mod( iter, plot_step ) == 0
+        idx     = iter / plot_step;
+        xx(idx) = iter;
+        yy(idx) = mean( abs(R -(W>0.5))); 
+        
+        plot(xx,yy);
+        xlabel('iter');            
+        ylabel('Acc_Train' );
+        drawnow;
+    end
+    % calculate gradient
+    dB    = dB +  (E * G') / h - wd * B;
+    dbias = dbias + sum( E )/h;
+    
+    % update weight
+    B = B + learning_rate * dB;
+    bias = bias + learning_rate * dbias;
+
+    dB = dB * momentum;
+    dbias = dbias * momentum; 
+
+    dbias
+    nm_dB = norm( dB )
+end
+
+
+
+
+
