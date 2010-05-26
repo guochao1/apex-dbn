a2d0e1a2a81fa33373d4ea0bb52a2df87c986098
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-26 02:19:58 +0000

    add experiment of detection

--- matlab/hog_exp/work.m
+++ matlab/hog_exp/work.m
@@ -1,4 +1,11 @@
-step_make_class_data;
-step_make_class_feature;
-step_make_train_svm;
-step_make_test_svm;
+model = load_crbm_model('00.kyoto3.modify.model');
+img   = im2double( rgb2gray(imread(['/home/crow/ML_exp/data_set/' ...
+                    'INRIAPerson/Test/pos/crop001504.png'])));
+
+
+Wou   = crbm_filter( img, model.layer{1} );
+Wp    = crbm_pool  ( Wou, model.layer{1} );
+ 
+igw   = draw_tensor( Wp,'local' );
+
+imshow( igw );
