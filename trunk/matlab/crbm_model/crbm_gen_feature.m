8c0f86fb7052e39a2d4032684453405beb5d8612
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-26 12:10:54 +0000

    x

--- trunk/matlab/crbm_model/crbm_gen_feature.m
+++ trunk/matlab/crbm_model/crbm_gen_feature.m
@@ -1,5 +1,13 @@
 function F = crbm_gen_feature( img, model )
-F = im2double(rgb2gray(img));
+
+[y_max,x_max,z_max] = size(img);
+
+if z == 3
+    F = im2double(rgb2gray(img));
+else
+    F = img;
+end
+
 for i = 1 : length( model.layer )
     F = crbm_filter( F,model.layer{i} );
     F = crbm_pool  ( F,model.layer{i} );
