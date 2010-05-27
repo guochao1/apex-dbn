81b32966d9b82aa968a8c8ad8c697e7331ef210f
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-27 01:59:53 +0000

    add hard neg example

---
+++ trunk/matlab/hog_exp/script/step_gen_detection_list.m
@@ -0,0 +1,16 @@
+% default parameter for HOG
+df_param.c_y_max = 8;
+df_param.c_x_max = 8;
+df_param.b_y_max = 2;
+df_param.b_x_max = 2;
+df_param.num_hist= 9;
+df_param.slide_step = 1;
+df_param.signed = 'unparam.signed';
+df_param.norm_method = 'l1sqrt';
+df_param.w_y_max = 30;
+df_param.w_x_max = 14;
+
+
+
+fname_model = '';
+fname_img   = '';
