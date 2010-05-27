408d4d6cc4577dd965ef0268be9a9c9c9b13f8b2
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-05-27 12:54:00 +0000

    x

--- matlab/hog_exp/gen_detection_list_crbm_filter_linear.m
+++ matlab/hog_exp/gen_detection_list_crbm_filter_linear.m
@@ -10,12 +10,12 @@ end
 %
 [d_y_max,d_x_max] = crbm_get_feature_size( 128,64, crbm_model );
 for i = 1 : length( fnamelist ) 
-    fname = strcat( dirname, fnamelist{i}.name );
+    fname = strcat( dirname, fnamelist(i).name );
     img = imread( fname );
     [Y,score] = gen_detection_crbm_filter_linear( img, crbm_model,...
                                                   B  , bias,...
                                                   d_y_max, d_x_max );
     dlst{i}.Y = Y( :, score > c );
     dlst{i}.score = score( :, score>c );
-    fprintf(1,'%d:%s\n', i, fnamelist{i}.name );   
+    fprintf(1,'%d:%s\n', i, fnamelist(i).name );   
 end
---
+++ matlab/hog_exp/gen_detection_list_crbm_linear.m
@@ -0,0 +1,22 @@
+function dlst = gen_detection_list_crbm_linear( dirname, fnamelist,...
+                                                crbm_model, ...
+                                                B, bias, c ...
+                                                )
+if nargin < 6
+    c = 0;
+end
+
+% generate list of detections for certain image
+%
+[d_y_max,d_x_max] = crbm_get_feature_size( 128,64, crbm_model );
+
+for i = 1 : length( fnamelist ) 
+    fname = strcat( dirname, fnamelist(i).name );
+    img = imread( fname );
+    [Y,score] = gen_detection_crbm_linear( img, crbm_model,...
+                                           B  , bias,...
+                                           d_y_max, d_x_max );
+    dlst{i}.Y = Y( :, score > c );
+    dlst{i}.score = score( :, score>c );
+    fprintf(1,'%d:%s\n', i, fnamelist(i).name );   
+end
