5442f049eb0d54fd51615d90d511360f3aee34e2
workcrow <workcrow@b861ab8a-2dba-11df-8e64-fd3be38ee323>
2010-07-28 02:18:37 +0000

    add dSFMT as prng

--- external/apex_random.h
+++ external/apex_random.h
@@ -4,18 +4,7 @@
 /*   generate random number using Mersene Twister */
 
 extern "C"{
-
-#ifdef _MSC_VER
-// we don't use SSE for safety concern
-#else
-#define HAVE_SSE2
-#endif
-
-#define MEXP  19937
-#include "SFMT/SFMT.c"
-
-#undef HAVE_SSE2
-#undef MEXP
+#include "dSFMT/dSFMT.h"
 }
 
 #include <cmath>
@@ -31,30 +20,30 @@ namespace apex_random{
 
 	/*-------interface code------*/
 	inline void seed( uint32_t seed ){
-		init_gen_rand( seed ); 
+		dsfmt_gv_init_gen_rand( seed );
 	}
 		
 	/* return a 32 bit random number */
 	inline uint32_t next_uint32(){
-		return gen_rand32();
+		return dsfmt_gv_init_gen_rand();
 	}
 	
 	/*-------------------------------*/
 
 	/* return a random number in n */
 	inline uint32_t next_uint32( uint32_t n ){
-		return (uint32_t) ( (double)next_uint32() * ( 1.0 / 4294967296.0 ) * n ) ;
+		return (uint32_t) ( next_double() * n ) ;
 	}
 	
 	/* return a real number uniform in [0,1) */
 	inline double next_double(){
-		return next_uint32() * (1.0/4294967296.0); 
+		return dsfmt_gv_genrand_close_open();
 	}
-	/* return a real number uniform in (0,1) */
-	inline double next_double2(){
-		return ( (double)next_uint32() + 0.5) * (1.0/4294967296.0); 
-	}
-	
+	/* return a real numer uniform in (0,1) */
+    inline double next_double2(){
+        return dsfmt_gv_genrand_open_open();
+    }
+
 	/* return  x~N(0,1) */
 	inline double sample_normal(){
 		double x,y,s;
@@ -88,19 +77,6 @@ namespace apex_random{
 	inline int sample_binary( double p ){
 		return next_double() <  p;  
 	}
-
-	/* sample a given distribution p */
-	inline int sample_distribution( const double *p, int n ){
-		double t   = next_double();
-		double sum = 0;
-		int i = 0;
-
-		do{
-			sum += p[ i++ ];
-		}while( t > sum && i < n );
-		
-		return i - 1;
-	}	
 };
 
 #endif
